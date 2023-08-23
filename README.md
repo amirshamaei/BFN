0769159d2b5d45562477c15f25c301cc5fa48d7a
amir_shamaei <shamaie.amir@gmail.com>
2023-08-23 13:02:24 +0200

    model has been tested.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-A naive implementation of [Bayesian Flow Networks](https://arxiv.org/abs/2308.07037)
+# A naive implementation of [Bayesian Flow Networks](https://arxiv.org/abs/2308.07037)
+
+---
+
+Algorithm 5 of the paper has been implemented [Continuous-Time Loss L∞(x) for Discretised Data]. I am looking forward to add all, but I'd be happy if you can contribute.
--- model.py
+++ model.py
@@ -2,76 +2,83 @@ import torch
 from torch import nn
 from torch.distributions import Uniform, Normal
 
-class DiscretisedDiffusion(nn.Module):
+class DiscretisedBNF(nn.Module):
 
-    def __init__(self, sigma1, K, hiddenDim):
+    def __init__(self, inDim, sigma1, K, hiddenDim):
         super().__init__()
+        self.inDim = inDim
         self.sigma1 = sigma1
         self.K = K
         self.NN = nn.Sequential(
-            nn.Linear(2*K + 1, hiddenDim),
+            nn.Linear(inDim + 1, hiddenDim),
             nn.LeakyReLU(),
-            nn.Linear(hiddenDim, 2*K)
+            nn.Linear(hiddenDim, 2*inDim)
         )
 
     def discretised_cdf(self,mu, sigma, x):
         F = 0.5 * (1 + torch.erf((x - mu) / (sigma * torch.sqrt(torch.tensor(2)))))
-        G = torch.where(x <= -1, torch.tensor(0),
-                        torch.where(x >= 1, torch.tensor(1), F))
+        if x<=-1:
+            G = torch.Tensor([0])
+        elif x>=1:
+            G = torch.Tensor([1])
+        else:
+            G = F
         return G
 
     import torch
     from torch import nn
 
-    def discretised_output_distribution(self, mu, t, K, gamma, tmin=1e-10):
+    def discretised_output_distribution(self, mu, t, gamma, tmin=1e-10):
 
-        D = mu.shape[0]
+        D = mu.shape[1]
 
         if t < tmin:
             mu_x = torch.zeros_like(mu)
             sigma_x = torch.ones_like(mu)
         else:
             # Run network to get mu_epsilon and ln(sigma_epsilon)
-            mu_epsilon, ln_sigma_epsilon = self.NN(torch.cat([mu, t]))
+            mu_epsilon, ln_sigma_epsilon = torch.split(self.NN(torch.cat([mu, t],dim=1)), self.inDim, dim=1)
 
-            mu_x = mu ** (gamma - 1 / (1 - gamma)) * (mu_epsilon ** (1 / (1 - gamma)))
-            sigma_x = (1 - gamma) ** (-1 / 2) * torch.exp(ln_sigma_epsilon / 2)
+            mu_x = (mu/gamma) - torch.sqrt((1-gamma)/(gamma)) * (mu_epsilon)
+            sigma_x = torch.sqrt((1-gamma)/(gamma))*torch.exp(ln_sigma_epsilon)
 
         pO = []
-        for d in range(D):
-            pO.append([])
-            for k in range(K):
-                kl = 2 * (k - 1) / (K - 1)
-                kr = 2 * k / (K - 1)
-                cdf_kl = self.discretised_cdf(mu_x[d], sigma_x[d], kl)
-                cdf_kr = self.discretised_cdf(mu_x[d], sigma_x[d], kr)
-                pO[d].append(cdf_kr - cdf_kl)
 
-        return torch.stack(pO)
+        for d in range(D):
+            p_temp = []
+            for k in range(self.K):
+                kl = (2 * (k - 1) / self.K) - 1
+                kr = (2 * k / self.K) - 1
+                cdf_kl = self.discretised_cdf(mu_x[:,d], sigma_x[:,d], kl)
+                cdf_kr = self.discretised_cdf(mu_x[:,d], sigma_x[:,d], kr)
+                p_temp.append(cdf_kr - cdf_kl)
+            pO.append(torch.stack(p_temp))
+
+        return torch.sum(torch.stack(pO),1).T
 
     def discretised_posterior(self, x):
-        t = Uniform(0, 1).sample()
+        t = Uniform(0, 1).sample((x.shape[0],1))
         gamma = 1 - self.sigma1 ** 2 * t
         mu = Normal(gamma * x, gamma * (1 - gamma)).sample()
         pO = self.discretised_output_distribution(mu, t, gamma)
         return pO
 
-    def iterative_process(self, n):
-        mu = torch.zeros(1)
-        rho = torch.ones(1)
+    def iterative_sampling_process(self,inDim=100 , n=100):
+        mu = torch.zeros((1,inDim))
+        rho = torch.ones((1,inDim))
         for i in range(1, n + 1):
-            t = (i - 1) / n
-            gamma = 1 - self.sigma1 ** 2 * t
-            k = self.discretised_posterior(mu)
-            alpha = self.sigma1 ** -2 * (i / n) * (1 - self.sigma1 ** 2 / n)
-            y = Normal(k, alpha).sample()
-            mu = (i - 1) / i * mu + y / i
-            rho = self.sigma1 ** 2 * (1 - 1 / i ** 2)
-        return mu, rho
+            t = torch.ones((1,1))*((i - 1) / n)
+            k = self.discretised_output_distribution(mu,t,1-(self.sigma1**(2*t)))
+            alpha = self.sigma1 ** -2 * (i / n) * (1 - self.sigma1 ** (2 / n))
+            y = Normal(k, 1/alpha).sample()
+            mu = ((rho*mu)+(alpha*y))/(rho+alpha)
+            rho = rho + alpha
+        k = self.discretised_output_distribution(mu, torch.ones((1,1)), 1 - (self.sigma1 ** (2 * t)))
+        return k
+model = DiscretisedBNF(inDim=100 ,sigma1=0.5, K=10, hiddenDim= 16)
 
-model = DiscretisedDiffusion(sigma1=0.5, K=10)
-
-x = torch.rand(100)
+x = torch.rand((1,100))
 posterior = model.discretised_posterior(x)
 
-mu, rho = model.iterative_process(n=100)
+k = model.iterative_sampling_process(inDim=100 ,n=100)
+print(k)
